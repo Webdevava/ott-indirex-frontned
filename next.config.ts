f391f30110801db69d569872df24e9ef24891ff7
Ankur Auti <ankur.auti@inditronics.com>
2025-09-19 11:13:00 +0530

    Add s3 urls config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "indi-test-img-processing.s3.ap-south-1.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "apm-captured-images.s3.ap-south-1.amazonaws.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
